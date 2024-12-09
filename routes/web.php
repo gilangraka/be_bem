42f0de4c58e8f34a86df79887aad32233471b247
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-12-09 22:22:12 +0700

    feat: add link

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('admin');
+});
+
+Route::get('link', function () {
+    Artisan::call('storage:link');
+    return 1;
 });
